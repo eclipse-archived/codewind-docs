ae58d6c0657e2796a2140aa0e3362dcc51dbddf9
James Cockbain <james.cockbain@ibm.com>
2020-06-25 10:32:09 +0100

    make sidenav retractable
    Signed-off-by: James Cockbain <james.cockbain@ibm.com>

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -14,19 +14,25 @@ function activeOSInstruction() {
 	$('.cw-tab-pane[data-os="'+os+'"]').addClass('active');
 }
 
+$('#sidebar-toggle').click(function () {
+	$('#content-container').toggleClass('full-width')
+	$('#sidebar-toggle').toggleClass('offcanvas')
+	$('#sidebar-icon').toggleClass('flipped')
+})
+
 $( document ).ready(function() {
-    var location = window.location;
+  var location = window.location;
 	var pathname = location.pathname.substring(location.pathname.lastIndexOf('/') + 1);
-	
+
 	// Syncs the sidebar to the current page
 	$('#sidebar-container a').each(function( i ) {
-		if ($(this)[0].hasAttribute("data-url")){			
+		if ($(this)[0].hasAttribute("data-url")){
 
 
 			if($(this).attr('data-url') === pathname){
 				// Add the class to highlight the selected item in the sidebar
 				$(this).addClass("cw-current-page");
-				
+
 				// Our sidebar supports three levels, so need to iterate up to open up
 				// the menus so the selected item can be shown
 				var parentID = $(this).attr("data-parent");
@@ -36,7 +42,10 @@ $( document ).ready(function() {
 					var grandparentID = "[id=\"" + parentID + "\"]";
 					if (grandparentID && grandparentID != ""){
 						var grandparent =  $(grandparentID).attr("data-parent");
-						$(grandparent).collapse();
+						// We don't want to collapse sidebar itself
+						if (grandparent != "#sidebar-container") {
+							$(grandparent).collapse();
+						}
 					}
 				}
 				// Stop the .each as we found the required URL
